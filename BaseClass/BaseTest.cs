3c65c9cc9137851d9ac65598905fcdafd66bc756
Sriram Yalla <y.sriram2@gmail.com>
2021-09-29 13:05:42 +0530

    Add files via upload

---
+++ BaseClass/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Collections;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnit.Tests3.BaseClass
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        public IWebDriver driver;
+        [OneTimeSetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            // TODO: Add your test code hiere
+            driver.Url = "https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=true&rm=false&" +
+                "continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&" +
+                "osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
+        }
+        [OneTimeTearDown]
+        public void Close()
+        {
+
+            driver.Close();
+        }
+    }
+}
---
+++ BaseClass/BaseTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Collections;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnit.Tests3.BaseClass
+{
+    [TestFixture]
+    public class BaseTest1
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            // TODO: Add your test code hiere
+            driver.Url = "https://www.youtube.com/";
+        }
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+
+        }
+    }
+}
